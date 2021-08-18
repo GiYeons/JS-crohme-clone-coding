7174b662d3aa18bfae331bb82568d9c15e73d211
quaster-me <65033360+quaster-me@users.noreply.github.com>
2021-08-18 16:12:07 +0900

    4. Login 끝
    로그아웃 버튼까지 추가했으나 한번씩 로그인이 되지 않고 새로고침되는 현상이 있음. 오류는 The 'x-ua-compatible' meta element should not be specified as it is not needed.

--- momentum/app.js
+++ momentum/app.js
@@ -2,15 +2,39 @@ const loginInput = document.querySelector("#login-form input");
 const loginForm = document.querySelector("#login-form");
 const greeting = document.querySelector("#greeting");
 
+const logoutForm = document.querySelector("#logout-form");
+
 const HIDDEN_CN = "hidden";
+const USERNAME_KEY = "username";
 
 function onLoginSubmit(event) {
   event.preventDefault();
-  const username = loginInput.value;
   loginForm.classList.add(HIDDEN_CN);
-  console.log(username);
-  greeting.innerText = `Hello ${username}`;
-  greeting.classList.remove(HIDDEN_CN);
+  localStorage.setItem(USERNAME_KEY, loginInput.value);
+  paintGreeting();
 }
 
-loginForm.addEventListener("submit", onLoginSubmit);
+function paintGreeting() {
+  greeting.classList.remove(HIDDEN_CN);
+  greeting.innerText = `Hello ${localStorage.getItem(USERNAME_KEY)}`;
+
+  logoutForm.classList.remove(HIDDEN_CN);
+  logoutForm.addEventListener("submit", onLogoutSubmit);
+}
+
+function onLogoutSubmit(event) {
+  event.preventDefault();
+  localStorage.removeItem(USERNAME_KEY, savedUsername);
+  greeting.classList.add(HIDDEN_CN);
+  logoutForm.classList.add(HIDDEN_CN);
+  loginForm.classList.remove(HIDDEN_CN);
+}
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if (savedUsername === null) {
+  loginForm.classList.remove(HIDDEN_CN);
+  loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+  paintGreeting();
+}
