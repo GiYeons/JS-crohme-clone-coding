1b55106ee10300b5a2f8953e8b302dcabc85eba4
quaster-me <65033360+quaster-me@users.noreply.github.com>
2021-09-01 14:21:47 +0900

    날씨까지 완료
    API 사용해보기

---
+++ momentum/js/weather.js
@@ -0,0 +1,20 @@
+const API_KEY = "fd2d760113af2c9e2aebbcf475e2bdc1";
+
+function onGeoOk(position) {
+  const lat = position.coords.latitude;
+  const long = position.coords.longitude;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${API_KEY}&units=metric`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      const weather = document.querySelector("#weather span:first-child");
+      const city = document.querySelector("#weather span:last-child");
+      weather.innerText = data.name;
+      city.innerText = data.weather[0].main;
+    });
+}
+function onGeoError() {
+  alert("Can't find you. No weather for you.");
+}
+
+navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
